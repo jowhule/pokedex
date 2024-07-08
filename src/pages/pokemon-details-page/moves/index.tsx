e5723c51ff77cba20336dfe8e59e96ca0ead1654
Jo Wu <jo.wu@student.unsw.edu.au>
2024-07-09 08:11:37 +1000

    added file for moves

---
+++ src/pages/pokemon-details-page/moves/index.tsx
@@ -0,0 +1,18 @@
+/* move category -> go through all move
+ -> store each in either learn method: level-up, machine, tutor
+ -> apply filter of region??? but then i need to knmow what all 
+ regions available... and keep a store of that, which means
+ for region i store each learn method
+
+ !! maybe query for move dmg, element type, special/phys, accuracy, pp
+
+ structure:?
+  {regions: {
+    level-up: {move: level-learned at}, 
+    machine: [move1, move2], 
+    tutor: [move1, move2]}
+  }
+
+ */
+
+export const hi = "hi";
